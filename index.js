621d6f1431fc2309ce6d00265f7b72209f41f85a
maksim.korzhov <maksim.korzhov@geidans.co.uk>
2021-02-17 22:36:26 +0300

    Added async/await in the loop examples

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // firstExample();
 // secondExample();
 // thirdExample();
-asyncExample();
+// asyncExample();
+mapAsyncExample();
 
 function firstExample() {
 	console.log("Priority of the tasks with no time delay.");
@@ -152,6 +153,78 @@ function asyncExample() {
 	makeSmoothySlow().then(val => { console.log(val); console.timeLog("s1"); });
 
 	console.time("s2");
-	makeSmoothy().then(val => { console.log(val); console.timeLog("s2"); });
+	makeSmoothy().then(val => { console.log(val); console.timeLog("s2"); });	
+}
+
+// ATTENTION!
+// Await pause only async function execution, not all program.
+function mapAsyncExample() {
+	// Every function with an async keyword returs Promise as a result.
+	const getFruit = async (fruitName) => {
+		const fruits = {
+			"apple": "🍏",
+			"pineapple": "🍍",
+			"peach": "🍑",
+			"strawberry": "🍓",
+		};
+
+		await new Promise(resolve => {
+			setTimeout(resolve, Math.random() * 1000);
+		});
+
+		return fruits[fruitName];
+	};
+
+	const listFruits = ["apple", "pineapple", "peach", "strawberry"];
+
+	// If we want to iterate through await there are few ways
 	
+	// 1. Resolve all awaits using Promise.all
+	// All tasks are running in parallel.
+	const parallelAsync = async () => {
+		const fruitPromises = listFruits.map(v => getFruit(v));
+
+		// Promise.all returns all results in the correct order,
+		// but it doesn't guaranted that all promises was executed in the same order.
+		const fruitsIcons = await Promise.all(fruitPromises);
+		console.log("fi", fruitsIcons);
+	}
+
+	//parallelAsync();
+
+	// 2. The same behaviour if we use forEach or map loop.
+	// It is doen't really pause the function in this context.
+	// All promises here will be running concurrently.
+	// It DOES NOT save the order like Promise.all,
+	// so the result will be in random order.
+	listFruits.map(async v => {
+		const emoji = await getFruit(v);
+		//console.log(emoji);
+		return emoji;
+	});
+
+	// 3. The only way to use await in the loop - is the for...of loop.
+	// It is waits the previuos iteration end before start new await.
+	const fruitLoop = async () => {
+		console.time("forOfFruitLoop");
+		for ( const c of listFruits ) {
+			const emoji = await getFruit(c);
+			console.log(emoji);
+			console.timeLog("forOfFruitLoop");
+		}
+	};
+
+	// fruitLoop();
+
+	// 4. We can use await inside the for...of declaration
+	const fruitPromises2 = listFruits.map(v => getFruit(v));
+	const fruitLoop2 = async () => {
+		console.time("forOfFruitLoop2");
+		for await ( const emoji of fruitPromises2 ) {
+			console.log(emoji);
+			console.timeLog("forOfFruitLoop2");
+		}
+	};
+
+	fruitLoop2();
 }
